dab4068f35f513408ae21f35e224ac6ec3fb3c3c
kazu5470 <rad.kazu08060806@gmail.com>
2024-06-13 01:49:34 +0900

    pages配下に適当にディレクトリ掘って作ってみた

---
+++ nameless-react-app/src/components/Home/index.tsx
@@ -0,0 +1,5 @@
+import { Text } from '@mantine/core';
+
+export const Home = () => {
+  return <Text>これはHomeのページです</Text>;
+};
---
+++ nameless-react-app/src/components/Top/index.tsx
@@ -0,0 +1,5 @@
+import { Text } from '@mantine/core';
+
+export const Top = () => {
+  return <Text>TOPページ</Text>;
+};
